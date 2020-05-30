5bbecdf134dd63697c36d97f27cdfb6167bba5fb
etff <wish2rich@gmail.com>
2020-05-30 22:12:56 +0900

    feature(Member): add update information

--- src/main/java/com/employee/app/Member/domain/Member.java
+++ src/main/java/com/employee/app/Member/domain/Member.java
@@ -1,5 +1,6 @@
 package com.employee.app.Member.domain;
 
+import com.employee.app.dto.MemberDto;
 import lombok.*;
 
 @Builder
@@ -8,9 +9,18 @@ import lombok.*;
 @Getter
 @RequiredArgsConstructor
 public class Member {
-    private final Id id;
-    private final Name name;
-    private final Email email;
-    private final Phone phone;
-    private final Rank rank;
+    private Id id;
+    private Name name;
+    private Email email;
+    private Phone phone;
+    private Rank rank;
+
+    public Member changeInformation(MemberDto memberDto) {
+        this.name = memberDto.getName();
+        this.phone = memberDto.getPhone();
+        this.email = memberDto.getEmail();
+        this.rank = memberDto.getRank();
+
+        return this;
+    }
 }
--- src/main/java/com/employee/app/dto/MemberDto.java
+++ src/main/java/com/employee/app/dto/MemberDto.java
@@ -1,15 +1,16 @@
 package com.employee.app.dto;
 
-import com.employee.app.Member.domain.Rank;
+import com.employee.app.Member.domain.*;
 import lombok.Builder;
 import lombok.Getter;
 
 @Builder
 @Getter
 public class MemberDto {
-    private final String id;
-    private final String name;
-    private final String email;
-    private final String phone;
+    private final Id id;
+    private final Name name;
+    private final Email email;
+    private final Phone phone;
     private final Rank rank;
+
 }
