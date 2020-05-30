1ba2941f9a1d9405f8288db3e4d83626b304eb70
etff <wish2rich@gmail.com>
2020-05-30 20:17:37 +0900

    refactor(Member): add hash and equals

--- src/main/java/com/employee/app/Member/domain/Member.java
+++ src/main/java/com/employee/app/Member/domain/Member.java
@@ -2,8 +2,7 @@ package com.employee.app.Member.domain;
 
 import lombok.Builder;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import java.util.Objects;
 
 @Builder
 public class Member {
@@ -13,17 +12,31 @@ public class Member {
     private final Phone phone;
     private final Rank rank;
 
-    public Member() {
-        this(null, null,null, null, null);
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Member)) return false;
+        Member member = (Member) o;
+        return Objects.equals(id, member.id) &&
+                Objects.equals(name, member.name) &&
+                Objects.equals(email, member.email) &&
+                Objects.equals(phone, member.phone) &&
+                rank == member.rank;
     }
 
-    public Member(String id, Name name, Email email, Phone phone, Rank rank) {
-        this.id = id;
-        this.name = name;
-        this.email = email;
-        this.phone = phone;
-        this.rank = rank;
+    @Override
+    public String toString() {
+        return "Member{" +
+                "id='" + id + '\'' +
+                ", name=" + name +
+                ", email=" + email +
+                ", phone=" + phone +
+                ", rank=" + rank +
+                '}';
     }
 
-
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, email, phone, rank);
+    }
 }
