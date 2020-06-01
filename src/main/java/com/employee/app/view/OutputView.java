edd061aa9c1d7207fb226b7b5f272b34a560998c
etff <wish2rich@gmail.com>
2020-06-01 09:01:56 +0900

    refactor(outputView): add constant

--- src/main/java/com/employee/app/view/OutputView.java
+++ src/main/java/com/employee/app/view/OutputView.java
@@ -5,8 +5,22 @@ import com.employee.app.Member.domain.Members;
 public class OutputView {
     private static final int MENU_END_NUMBER = 0;
     private static final String DASH = "============";
-    public static final String TAB = "\t\t";
-    public static final String NOT_FOUND_USER = "사용자가 존재하지 않습니다.";
+    private static final String TAB = "\t\t";
+    private static final String NOT_FOUND_USER = "사용자가 존재하지 않습니다.";
+    private static final String NOT_MENU_NUMBER = "올바르지 않은 메뉴번호";
+    private static final String DETAIL_MEMBER_MENU = "직원번호\t 이름\t 전화번호\t 직급\t 이메일";
+    private static final String LONG_DASH = "=======================================";
+    private static final int ADD_MEMBER_MENU = 1;
+    private static final int PRINT_MEMBER_NUMBER = 2;
+    private static final int PRINT_MEMBER_DETAIL_NUMBER = 3;
+    private static final int UPDATE_MEMBER_INFO_NUMBER = 4;
+    private static final int REMOVE_MEMBER_NUMBER = 5;
+    private static final String SUBMENU_EXIT = "0. 종료";
+    private static final String SUBMENU_NEW_MEMBER = "1. 직원 정보 입력";
+    private static final String SUBMENU_MEMBER_LIST = "2. 직원 리스트";
+    private static final String SUBMENU_MEMBER_DETAILS = "3. 직원 상세 정보 출력";
+    private static final String SUBMENU_MEMBER_UPDATE = "4. 직원 정보 수정";
+    private static final String SUBMENU_MEMBER_REMOVE = "5. 직원 정보 삭제";
 
     public static void printConsole(Members members) {
         printSubMenu();
@@ -19,23 +33,23 @@ public class OutputView {
             }
 
             switch (menuNumber) {
-                case 1:
+                case ADD_MEMBER_MENU:
                     InputView.askMemberInfo(members);
                     break;
-                case 2:
+                case PRINT_MEMBER_NUMBER:
                     printMembers(members);
                     break;
-                case 3:
+                case PRINT_MEMBER_DETAIL_NUMBER:
                     printMemberDetails(members);
                     break;
-                case 4:
+                case UPDATE_MEMBER_INFO_NUMBER:
                     InputView.updateMemberInfo(members);
                     break;
-                case 5:
+                case REMOVE_MEMBER_NUMBER:
                     InputView.removeMember(members);
                     break;
                 default:
-                    throw new IllegalArgumentException("올바르지 않은 메뉴번호");
+                    throw new IllegalArgumentException(NOT_MENU_NUMBER);
             }
             printSubMenu();
         }
@@ -43,12 +57,12 @@ public class OutputView {
 
     public static void printSubMenu() {
         StringBuilder sb = new StringBuilder();
-        sb.append("0. 종료").append(System.lineSeparator());
-        sb.append("1. 직원 정보 입력").append(System.lineSeparator());
-        sb.append("2. 직원 리스트").append(System.lineSeparator());
-        sb.append("3. 직원 상세 정보 출력").append(System.lineSeparator());
-        sb.append("4. 직원 정보 수정").append(System.lineSeparator());
-        sb.append("5. 직원 정보 삭제").append(System.lineSeparator());
+        sb.append(SUBMENU_EXIT).append(System.lineSeparator());
+        sb.append(SUBMENU_NEW_MEMBER).append(System.lineSeparator());
+        sb.append(SUBMENU_MEMBER_LIST).append(System.lineSeparator());
+        sb.append(SUBMENU_MEMBER_DETAILS).append(System.lineSeparator());
+        sb.append(SUBMENU_MEMBER_UPDATE).append(System.lineSeparator());
+        sb.append(SUBMENU_MEMBER_REMOVE).append(System.lineSeparator());
         System.out.println(sb.toString());
     }
 
@@ -73,8 +87,8 @@ public class OutputView {
             System.lineSeparator();
         } else {
             System.out.println();
-            System.out.println("직원번호\t 이름\t 전화번호\t 직급\t 이메일");
-            System.out.println("=======================================");
+            System.out.println(DETAIL_MEMBER_MENU);
+            System.out.println(LONG_DASH);
 
             members.getMembers().forEach(member -> {
                 System.out.println(
