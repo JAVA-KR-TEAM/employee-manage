65a6778eccff1c24dfafb11e9cb1432b8ca50b2c
Rebwon <msolo021015@naver.com>
2020-06-01 16:31:32 +0900

    refactor line separator

--- src/main/java/com/employee/app/view/ConsoleOutput.java
+++ src/main/java/com/employee/app/view/ConsoleOutput.java
@@ -3,17 +3,17 @@ package com.employee.app.view;
 import com.employee.app.domain.Employees;
 
 public class ConsoleOutput implements OutputView {
-	private static final String lineBreak = "\n";
 	private static final String longDash = "===============";
 	private static final String DETAIL_EMPLOYEES_MENU = "직원번호\t이름\t전화번호\t\t\t직급\t이메일";
 	private static final String EMPLOYEES_MENU = "직원번호\t이름";
 	private static final String EXIT = "프로그램을 종료합니다.";
+	private static final String lineSeparator = System.lineSeparator();
 
 	@Override
 	public void employeeList(Employees employees) {
 		StringBuilder sb = new StringBuilder();
-		sb.append(lineBreak);
-		sb.append(EMPLOYEES_MENU + lineBreak);
+		sb.append(lineSeparator);
+		sb.append(EMPLOYEES_MENU + lineSeparator);
 		sb.append(longDash);
 		System.out.println(sb.toString());
 		employees.findEmployees();
@@ -22,8 +22,8 @@ public class ConsoleOutput implements OutputView {
 	@Override
 	public void employeeDetailList(Employees employees) {
 		StringBuilder sb = new StringBuilder();
-		sb.append(lineBreak);
-		sb.append(DETAIL_EMPLOYEES_MENU + lineBreak);
+		sb.append(lineSeparator);
+		sb.append(DETAIL_EMPLOYEES_MENU + lineSeparator);
 		sb.append(longDash).append(longDash).append(longDash).append(longDash);
 		System.out.println(sb.toString());
 		employees.findDetailEmployees();
