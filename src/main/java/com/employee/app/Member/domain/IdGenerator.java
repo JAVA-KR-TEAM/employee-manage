907a2b6ed74d1726940f475b904455d91ea95d2e
etff <wish2rich@gmail.com>
2020-06-01 21:03:56 +0900

    refactor(IdGenerator): add private constructor

--- src/main/java/com/employee/app/Member/domain/IdGenerator.java
+++ src/main/java/com/employee/app/Member/domain/IdGenerator.java
@@ -3,8 +3,11 @@ package com.employee.app.Member.domain;
 import com.employee.app.utils.StringUtils;
 
 public class IdGenerator {
-    public static final String CAN_NOT_CREATE_ID = "더 이상 아이디를 만들 수 없습니다.";
+    private static final String CAN_NOT_CREATE_ID = "더 이상 아이디를 만들 수 없습니다.";
     private static int count = 0;
+
+    private IdGenerator() {
+    }
 
     public static Id generate() {
         if (count > 999) {
