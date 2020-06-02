d145d5698d8a0fdb12c135a079f877439043f11b
MaengSol <msolo021015@naver.com>
2020-06-02 10:59:00 +0900

    Revert "안녕하세요, 직원 관리 프로그램 리뷰요청드립니다."

--- src/main/java/com/employee/app/Application.java
+++ src/main/java/com/employee/app/Application.java
@@ -1,13 +1,4 @@
 package com.employee.app;
 
-import com.employee.app.Member.domain.Employees;
-
-import static com.employee.app.view.OutputView.printConsole;
-
-
 public class Application {
-
-    public static void main(String[] args) {
-        printConsole(new Employees());
-    }
 }
